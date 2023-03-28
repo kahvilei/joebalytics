c3976edce68257b40a94817c93dbe6a4b86dc2d8
Katie Huff <luovakatie@gmail.com>
2023-03-28 17:48:38 -0400

    move

--- server/app.js
+++ server/app.js
@@ -13,6 +13,28 @@ const summoners = require('./routes/summoners');
 const matches = require('./routes/matches');
 const user = require('./routes/user');
 
+const environment = process.env.ENVIRONMENT;
+
+let ENVIRONMENT_VARIABLES = {
+    'process.env.ENVIRONMENT': JSON.stringify('development'),
+    'process.env.PORT': JSON.stringify('3080'),
+    'process.env.MONGO_CONNECTION_STRING': JSON.stringify('mongodb://mongo-db:27017')
+  };
+  
+  if (environment === 'test') {
+    ENVIRONMENT_VARIABLES = {
+      'process.env.ENVIRONMENT': JSON.stringify('test'),
+      'process.env.PORT': JSON.stringify('3080'),
+      'process.env.MONGO_CONNECTION_STRING': JSON.stringify('mongodb://mongo-db:27017')
+    };
+  } else if (environment === 'production') {
+    ENVIRONMENT_VARIABLES = {
+      'process.env.ENVIRONMENT': JSON.stringify('production'),
+      'process.env.PORT': JSON.stringify('80'),
+      'process.env.MONGO_CONNECTION_STRING': JSON.stringify('mongodb+srv://admin123:admin123@todo-cluster.zpikr.mongodb.net/?retryWrites=true&w=majority')
+    };
+  }
+
 
 // Connect Database
 connectDB();
@@ -27,6 +49,8 @@ app.use('/api/summoners', summoners);
 app.use('/api/matches', matches);
 app.use('/api/user', user);
 
-const port = process.env.PORT || 8082;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
