4668ee428153d024684b2137d450b643ed810cbf
Katie Huff <luovakatie@gmail.com>
2024-03-07 17:24:28 -0500

    champ mastery styling

--- src/views/components/ChampMasteryRanking.js
+++ src/views/components/ChampMasteryRanking.js
@@ -23,10 +23,41 @@ function ChampMasteryRanking(props) {
     }, [id]);
 
     // returns a numbers of div elements depending on the number of tokens the summoner has for the champion
-    const tokenDisplay = (tokens) => {
+    const tokenDisplay = (tokens, masteryLevel, currentPoints) => {
+        const tokensNeeded = masteryLevel === 6 ? 3 : masteryLevel === 7 ? 0 : masteryLevel === 5 ? 2 : 0;
+        const pointsNeeded = masteryLevel === 1 ? 1800 : masteryLevel === 2 ? 6000 : masteryLevel === 3 ? 12600 : masteryLevel === 4 ? 21600 : 0;
+        const pointsRemaining = pointsNeeded - currentPoints;
+        //calculate points needed in between current level and next level
+        const pointScale = pointsNeeded - (masteryLevel === 1 ? 0 : masteryLevel === 2 ? 1800 : masteryLevel === 3 ? 6000 : masteryLevel === 4 ? 12600 : 21600);
+
+        //calculate current percentage of points needed to reach next level
+        const percentage = pointsRemaining / pointScale;
+        if(masteryLevel === 7){
+            return (
+                <div className='points-bar'>
+                    <div className="points-needed" key={0}>Max Mastery</div>
+                </div>
+            );
+        }
         let tokenArray = [];
-        for(let i = 0; i < tokens; i++){
+        let i = 0;
+        for(i = 0; i < tokens; i++){
             tokenArray.push(<div className="token" key={i}></div>);
+        }
+        const tokensMissing = tokensNeeded - tokens;
+        const currentCount = i;
+        for(i = 0; i < tokensMissing; i++){
+            tokenArray.push(<div className="token null" key={currentCount + i}></div>);
+        }
+        if(tokensNeeded === 0){
+            tokenArray.push(
+            <div className='points-bar'> 
+            <div className="scale">
+            <div className="bar" style={{width: `${100 - (percentage * 100)}%`}}></div>
+            </div>
+            <div className="points-needed" key={0}>{pointsRemaining} points remaining</div>
+            </div>
+            );
         }
         return tokenArray;
     }
@@ -34,7 +65,7 @@ function ChampMasteryRanking(props) {
     if(isLoading){
         return (  
         <div className='ChampMasteryRanking'>
-            <LoadingCircle/>
+            <LoadingCircle aspectRatio="flat-rectangle"/>
         </div>
         );
     }else{
@@ -44,10 +75,10 @@ function ChampMasteryRanking(props) {
                         return (
                             <div className="mastery-card summoner" key={index}>
                                 <img src={getSummonerIcon(summoner.profileIconId)} alt={summoner.summonerName} />
-                                <div>{summoner.summonerName}</div>
-                                <div className='tokens'>{tokenDisplay(summoner.tokensEarned)}</div>
-                                <div>Mastery {summoner.championLevel}</div>
-                                <div>{summoner.championPoints} points</div>
+                                <div className = "summoner-name">{summoner.summonerName}</div>
+                                <div className='tokens'>{tokenDisplay(summoner.tokensEarned, summoner.championLevel, summoner.championPoints)}</div>
+                                <div className='mastery'>Mastery {summoner.championLevel}</div>
+                                <div className='points'>{summoner.championPoints} points</div>
                             </div>
                         );
                     })}
